fc3711a65c57be846181264b746909139670d17c
PROF ROMULO <rjlpena@gmail.com>
2024-07-29 14:40:47 -0300

    aula 6 e 7

---
+++ script.js
@@ -0,0 +1,67 @@
+
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Como você acredita que podemos combater o racismo na sociedade?",
+        alternativas: [
+            {
+                texto: "Participando de campanhas de conscientização e educação sobre igualdade racial.",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Denunciando atos de discriminação e apoiando políticas públicas inclusivas.",
+                afirmacao: "afirmacao"
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Como você pode ajudar a combater a disseminação de fake news, especialmente as que perpetuam o racismo?",
+        alternativas: [
+            {
+                texto: "Verificando a veracidade das informações antes de compartilhar qualquer conteúdo, especialmente sobre temas sensíveis como racismo.", 
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "Educando amigos e familiares sobre os perigos das fake news e incentivando-os a não compartilhar conteúdos falsos que perpetuam o racismo.",
+                afirmacao: "afirmacao"
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Como podemos incentivar o uso de energia solar e outras formas de energia limpa, especialmente em comunidades carentes?",
+        alternativas: [
+            "Promovendo a instalação de painéis solares em escolas e centros comunitários de comunidades marginalizadas.", 
+            "Defendendo políticas públicas que subsidiem a energia solar para famílias de baixa renda."
+        ]
+    } 
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", function(){
+            atual ++;
+        })
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+
+mostraPergunta();
